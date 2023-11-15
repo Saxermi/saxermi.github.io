26c2540117639756f919fb0b934cc7bfc2aa1060
saxermi <137685346+Saxermi@users.noreply.github.com>
2023-11-15 16:36:28 +0100

    outcommented stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+//import remarkMath from 'remark-math';
+//import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,8 +15,8 @@ site: 'https://saxermi.github.io/',
 			wrap: false,
 		}
   },
-    integrations: [mdx({remarkPlugins: [remarkMath], // relevant
-			rehypePlugins: [rehypeKatex] // relevant
+    integrations: [mdx({//remarkPlugins: [remarkMath], // relevant
+			//rehypePlugins: [rehypeKatex] // relevant
 			}), sitemap(), tailwind()],
 });
 
