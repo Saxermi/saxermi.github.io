251b6dc1a7b25275459f53d694274ee4182321bb
saxermi <137685346+Saxermi@users.noreply.github.com>
2023-11-15 16:06:51 +0100

    inital commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+// https://astro.build/config
+export default defineConfig({
+site: 'https://saxermi.github.io/',
+
+  markdown: {
+    	shikiConfig: {
+			theme: "dracula",
+			wrap: false,
+		}
+  },
+    integrations: [mdx({remarkPlugins: [remarkMath], // relevant
+			rehypePlugins: [rehypeKatex] // relevant
+			}), sitemap(), tailwind()],
+});
+
---
+++ tag_backup_readfirst/readme.txt
@@ -0,0 +1,1 @@
+this tag belongs to project simmialr to tag in blog but it does not build so i took it out for the moment
